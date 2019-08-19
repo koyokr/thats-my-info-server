58c9b16c941eec64a2e847159c554e276e191115
koyo <koyo@koyo.kr>
2019-08-20 01:54:53 +0900

    후이즈 기능 구현 완료

--- src/main/java/com/rs/privacy/service/SolveService.java
+++ src/main/java/com/rs/privacy/service/SolveService.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.rs.privacy.model.SiteAdminContactDTO;
 import com.rs.privacy.model.SolveNaverDTO;
-import org.apache.commons.net.whois.WhoisClient;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -22,7 +21,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
-import java.net.SocketException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -36,13 +34,98 @@ public class SolveService {
     private final static String UMON_ADD_INTERFACE_URL = "https://help.naver.com/support/umon/umonAddInterface.help";
     private final static String REPORT_INQUIRY_INSERT_URL = "https://help.naver.com/support/mail/reportInquiryInsert.help";
 
-    private static final String WHOIS_SERVER_PATTERN = "Whois Server:\\s(.*)";
+    private final static String WHOIS_KISA_URL = "http://whois.kisa.or.kr/";
+    private final static String WHOIS_KISA_PUNYCODE_URL = "https://xn--c79as89aj0e29b77z.xn--3e0b707e/";
 
     @Autowired
     RestTemplateBuilder restTemplateBuilder;
 
     @Autowired
     ObjectMapper objectMapper;
+
+    public Boolean inNaver(SolveNaverDTO solveDTO) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Referer", REFERER_URL);
+
+        Document doc = getDocument();
+        if (doc == null) {
+            return false;
+        }
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("contentUrl", solveDTO.getUrl());
+        JsonNode node = restTemplate.postForObject(UMON_CHECK_INTERFACE_URL, new HttpEntity<>(map, headers), JsonNode.class);
+
+        String sendData = getSendData(node, solveDTO);
+        if (sendData == null) {
+            return false;
+        }
+        map = new LinkedMultiValueMap<>();
+        map.add("sendData", sendData);
+        map.add("ecmsCategoryNo", "12193");
+        map.add("reportToken", doc.getElementById("reportToken").attr("value"));
+        restTemplate.postForObject(UMON_ADD_INTERFACE_URL, new HttpEntity<>(map, headers), String.class);
+
+        String commonUrls = getCommonUrls(node, solveDTO);
+        if (commonUrls == null) {
+            return false;
+        }
+        List<String> keys = Arrays.asList(
+                "formToken", "reportToken", "attachFileNo", "attachFileCount", "categoryNo", "categoryAlias", "divisionExcept", "reply",
+                "noteAccept", "reinquiry", "firstUnacceptInquiryID", "serviceType", "location", "maxSizeOption", "hasRealName", "hasPhoneAuth",
+                "hasSignAuth", "onlyUmonUpdateFlag", "loggedIn", "itemCount", "answerType", "inquiryTitle",
+                "itemInfo[0]", "itemInfo[1]", "itemInfo[2]", "itemInfo[3]", "itemInfo[4]", "itemInfo[5]", "itemInfo[6]", "itemInfo[7]", "itemInfo[8]",
+                "itemInfo[9]", "itemInfo[10]", "itemInfo[11]", "itemInfo[12]", "itemInfo[13]", "itemInfo[14]", "itemInfo[15]", "itemInfo[16]",
+                "PcInqrPath"
+        );
+        map = new LinkedMultiValueMap<>();
+        addKeysByDoc(map, keys, doc);
+        map.add("reportCode", "AA24");
+        map.add("communityYn", "N");
+        map.add("specialistAnswer", "");
+        map.add("customerMemberID", "");
+        map.add("customerEmail", solveDTO.getEmail());
+        map.add("emailAddressChecked", "");
+        map.add("Email1", solveDTO.getEmail().split("@")[0]);
+        map.add("Email3", solveDTO.getEmail().split("@")[1]);
+        map.add("Email2", "self");
+        map.add("notLoginAccept", "N");
+        map.add("inquiryContent", "<span id=url>" + solveDTO.getUrl() + "</span><span id=opt>map.add(개인정보침해)</span>\n<span id=cont>" + solveDTO.getDescription() + "</span>\n");
+        map.add("service_code", "");
+        map.add("report_code", "");
+        map.add("umonContent", node.get("content").textValue());
+        map.add("umonStatus", node.get("status").textValue());
+        map.add("commonUrls", commonUrls);
+        map.add("url_input", solveDTO.getUrl());
+        map.add("_kin_id_input", "신고하시고자 하는 ID를 입력해주세요");
+        map.add("uMONtext01", solveDTO.getDescription());
+        map.add("attachCount_reportFile01", "0");
+        map.add("attachMaxCount_reportFile01", "3");
+        map.add("individualInfoYn", "Y");
+        map.add("parentAgreementNeed", "false");
+        String result = restTemplate.postForObject(REPORT_INQUIRY_INSERT_URL, new HttpEntity<>(map, headers), String.class);
+
+        return result.equals("SUCCESS");
+    }
+
+    public SiteAdminContactDTO getWhois(String url) {
+        Pattern pattern = Pattern.compile("(?:[\\w-]+\\.)?([\\w-]+?\\.(?:(?:co|or|pe|ne|re|go|hs|ms|es|kg|sc|ac)\\.)?[\\w-]+)");
+
+        String host = UriComponentsBuilder.fromHttpUrl(url).build().getHost();
+        Matcher matcher = pattern.matcher(host);
+        if (!matcher.find()) {
+            return null;
+        }
+        String domain = matcher.group(1);
+
+        String[] ds = domain.split("\\.");
+        String tld = ds[ds.length-1];
+        if (tld.equals("kr") || tld.equals("xn--3e0b707e")) {
+            return getContactFromKr(domain);
+        }
+        return getContactFromNotKr(domain);
+    }
 
     private void addKeysByDoc(MultiValueMap map, List<String> keys, Document doc) {
         for (String key : keys) {
@@ -112,125 +195,48 @@ public class SolveService {
         }
     }
 
-    public Boolean inNaver(SolveNaverDTO solveDTO) {
-        RestTemplate restTemplate = restTemplateBuilder.build();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Referer", REFERER_URL);
-
-        Document doc = getDocument();
-        if (doc == null) {
-            return false;
-        }
-
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("contentUrl", solveDTO.getUrl());
-        JsonNode node = restTemplate.postForObject(UMON_CHECK_INTERFACE_URL, new HttpEntity<>(map, headers), JsonNode.class);
-
-        String sendData = getSendData(node, solveDTO);
-        if (sendData == null) {
-            return false;
-        }
-        map = new LinkedMultiValueMap<>();
-        map.add("sendData", sendData);
-        map.add("ecmsCategoryNo", "12193");
-        map.add("reportToken", doc.getElementById("reportToken").attr("value"));
-        restTemplate.postForObject(UMON_ADD_INTERFACE_URL, new HttpEntity<>(map, headers), String.class);
-
-        String commonUrls = getCommonUrls(node, solveDTO);
-        if (commonUrls == null) {
-            return false;
-        }
-        List<String> keys = Arrays.asList(
-                "formToken", "reportToken", "attachFileNo", "attachFileCount", "categoryNo", "categoryAlias", "divisionExcept", "reply",
-                "noteAccept", "reinquiry", "firstUnacceptInquiryID", "serviceType", "location", "maxSizeOption", "hasRealName", "hasPhoneAuth",
-                "hasSignAuth", "onlyUmonUpdateFlag", "loggedIn", "itemCount", "answerType", "inquiryTitle",
-                "itemInfo[0]", "itemInfo[1]", "itemInfo[2]", "itemInfo[3]", "itemInfo[4]", "itemInfo[5]", "itemInfo[6]", "itemInfo[7]", "itemInfo[8]",
-                "itemInfo[9]", "itemInfo[10]", "itemInfo[11]", "itemInfo[12]", "itemInfo[13]", "itemInfo[14]", "itemInfo[15]", "itemInfo[16]",
-                "PcInqrPath"
-        );
-        map = new LinkedMultiValueMap<>();
-        addKeysByDoc(map, keys, doc);
-        map.add("reportCode", "AA24");
-        map.add("communityYn", "N");
-        map.add("specialistAnswer", "");
-        map.add("customerMemberID", "");
-        map.add("customerEmail", solveDTO.getEmail());
-        map.add("emailAddressChecked", "");
-        map.add("Email1", solveDTO.getEmail().split("@")[0]);
-        map.add("Email3", solveDTO.getEmail().split("@")[1]);
-        map.add("Email2", "self");
-        map.add("notLoginAccept", "N");
-        map.add("inquiryContent", "<span id=url>" + solveDTO.getUrl() + "</span><span id=opt>map.add(개인정보침해)</span>\n<span id=cont>" + solveDTO.getDescription() + "</span>\n");
-        map.add("service_code", "");
-        map.add("report_code", "");
-        map.add("umonContent", node.get("content").textValue());
-        map.add("umonStatus", node.get("status").textValue());
-        map.add("commonUrls", commonUrls);
-        map.add("url_input", solveDTO.getUrl());
-        map.add("_kin_id_input", "신고하시고자 하는 ID를 입력해주세요");
-        map.add("uMONtext01", solveDTO.getDescription());
-        map.add("attachCount_reportFile01", "0");
-        map.add("attachMaxCount_reportFile01", "3");
-        map.add("individualInfoYn", "Y");
-        map.add("parentAgreementNeed", "false");
-        String result = restTemplate.postForObject(REPORT_INQUIRY_INSERT_URL, new HttpEntity<>(map, headers), String.class);
-
-        return result == "SUCCESS";
-    }
-
-    private String getWhoisServer(String whoisData) {
-        String result = null;
-
-        Pattern pattern = Pattern.compile(WHOIS_SERVER_PATTERN);
-        Matcher matcher = pattern.matcher(whoisData);
-
-        while (matcher.find()) {
-            result = matcher.group(1);
-        }
-        return result;
-    }
-
-    private String queryWithWhoisServer(String domain, String whoisServerUrl) {
-        String result;
-
-        WhoisClient whois = new WhoisClient();
-        try {
-            whois.connect(whoisServerUrl);
-            result = whois.query(domain);
-            whois.disconnect();
-        } catch (SocketException e) {
-            return null;
-        } catch (IOException e) {
-            return null;
-        }
-        return result;
-    }
-
-    private final static String WHOIS_KISA_URL = "http://xn--c79as89aj0e29b77z.xn--3e0b707e/";
-
     private SiteAdminContactDTO getContactFromKr(String domain) {
-        RestTemplate restTemplate = restTemplateBuilder.build();
         String whoisUrl = UriComponentsBuilder.fromHttpUrl(WHOIS_KISA_URL)
                 .path("/openapi/whois.jsp")
                 .queryParam("query", domain)
                 .queryParam("key", "2019081917144773968679")
                 .queryParam("answer", "json")
                 .build().toUriString();
-        JsonNode node = restTemplate.getForObject(whoisUrl, JsonNode.class)
-                .get("whois").get("krdomain");
 
         SiteAdminContactDTO contact = new SiteAdminContactDTO();
+
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        String jsonString = restTemplate.getForObject(whoisUrl, String.class);
+        JsonNode node;
+        try {
+            node = objectMapper.readTree(jsonString).get("whois").get("krdomain");
+        } catch (IOException e) {
+            return contact;
+        } catch (NullPointerException e) {
+            return contact;
+        }
+
         contact.setName(node.get("adminName").textValue());
         contact.setEmail(node.get("adminEmail").textValue());
         contact.setRegistrar(node.get("agency").textValue());
         return contact;
     }
 
+    private String getMatcherGroupFirst(List<Pattern> patterns, String whoisResult) {
+        Matcher matcher;
+        for (Pattern pattern : patterns) {
+            matcher = pattern.matcher(whoisResult);
+            if (matcher.find()) {
+                return matcher.group(1);
+            }
+        }
+        return null;
+    }
+
     private SiteAdminContactDTO getContactFromNotKr(String domain) {
-        RestTemplate restTemplate = restTemplateBuilder.build();
-        String whoisUrl = UriComponentsBuilder.fromHttpUrl(WHOIS_KISA_URL)
+        String whoisUrl = UriComponentsBuilder.fromHttpUrl(WHOIS_KISA_PUNYCODE_URL)
                 .path("/kor/whois.jsc").build().toUriString();
-        String refererUrl = UriComponentsBuilder.fromHttpUrl(WHOIS_KISA_URL)
+        String refererUrl = UriComponentsBuilder.fromHttpUrl(WHOIS_KISA_PUNYCODE_URL)
                 .path("/kor/whois/whois.jsp").build().toUriString();
 
         HttpHeaders headers = new HttpHeaders();
@@ -239,33 +245,37 @@ public class SolveService {
 
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         map.add("query", domain);
+
+        RestTemplate restTemplate = restTemplateBuilder.build();
         String whoisResult = restTemplate.postForObject(whoisUrl, new HttpEntity<>(map, headers), String.class);
 
-        List<String> emailPatterns = Arrays.asList("Admin Email: (.+)", "Email: (.+)");
-        List<String> phonePatterns = Arrays.asList("Admin Phone: (.+)", "Phone: (.+)");
-
-        Pattern emailPattern = Pattern.compile("Admin Email: (.+)");
-        Matcher matcher = emailPattern.matcher(whoisResult);
+        List<Pattern> namePatterns = Arrays.asList(
+                Pattern.compile("Admin Name: (.+)"),
+                Pattern.compile("Tech Name: (.+)"),
+                Pattern.compile("Admin Organization: (.+)"),
+                Pattern.compile("Tech Organization: (.+)"),
+                Pattern.compile("Organization: (.+)")
+        );
+        List<Pattern> emailPatterns = Arrays.asList(
+                Pattern.compile("Admin Email: (.+)"),
+                Pattern.compile("Tech Email: (.+)"),
+                Pattern.compile("Email: (.+)")
+        );
+        List<Pattern> phonePatterns = Arrays.asList(
+                Pattern.compile("Admin Phone: (.+)"),
+                Pattern.compile("Tech Phone: (.+)"),
+                Pattern.compile("Phone: (.+)")
+        );
+        List<Pattern> registrarPatterns = Arrays.asList(
+                Pattern.compile("Registrar: (.+)")
+        );
 
         SiteAdminContactDTO contact = new SiteAdminContactDTO();
+        contact.setName(getMatcherGroupFirst(namePatterns, whoisResult));
+        contact.setEmail(getMatcherGroupFirst(emailPatterns, whoisResult));
+        contact.setPhone(getMatcherGroupFirst(phonePatterns, whoisResult));
+        contact.setRegistrar(getMatcherGroupFirst(registrarPatterns, whoisResult));
         return contact;
-    }
-
-    public SiteAdminContactDTO getWhois(String url) {
-        String host = UriComponentsBuilder.fromHttpUrl(url).build().getHost();
-        Pattern pattern = Pattern.compile("(?:[\\w-]+\\.)?([\\w-]+\\.[\\w-]+)");
-        Matcher matcher = pattern.matcher(host);
-        if (!matcher.find()) {
-            return null;
-        }
-
-        String domain = matcher.group(1);
-        String tld = domain.split(".")[1];
-
-        if (tld == "kr") {
-            return getContactFromKr(domain);
-        }
-        return getContactFromNotKr(domain);
     }
 
 }
