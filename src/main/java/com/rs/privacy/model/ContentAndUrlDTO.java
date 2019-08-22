a1e97b373821ce05ca5bfadd5a2985e07a04ba63
everpall <everpall@naver.com>
2019-08-23 05:22:42 +0900

    upload

---
+++ src/main/java/com/rs/privacy/model/ContentAndUrlDTO.java
@@ -0,0 +1,11 @@
+package com.rs.privacy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ContentAndUrlDTO {
+    private String content;
+    private String url;
+}
