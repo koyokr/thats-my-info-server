bbb17e0f8cd141ff27a5b8fc358451731421f8d2
koyo <koyo@koyo.kr>
2019-08-23 00:14:30 +0900

    new project

--- src/main/java/com/rs/privacyadmin/AdminApplication.java
+++ src/main/java/com/rs/privacyadmin/AdminApplication.java
@@ -3,19 +3,13 @@ package com.rs.privacyadmin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@EnableAdminServer
 @SpringBootApplication
-public class AdminApplication extends SpringBootServletInitializer {
+@EnableAdminServer
+public class AdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(AdminApplication.class);
-    }
 }
--- src/main/java/com/rs/privacyadmin/SecurityConfig.java
+++ src/main/java/com/rs/privacyadmin/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.rs.privacyadmin.config;
+package com.rs.privacyadmin;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
