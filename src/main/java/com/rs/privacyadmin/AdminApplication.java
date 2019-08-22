cba8c52cd507ad8f65c75065c0a3426516e8cc08
koyo <koyo@koyo.kr>
2019-08-22 22:24:35 +0900

    first commit

---
+++ src/main/java/com/rs/privacyadmin/AdminApplication.java
@@ -0,0 +1,21 @@
+package com.rs.privacyadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@EnableAdminServer
+@SpringBootApplication
+public class AdminApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AdminApplication.class);
+    }
+}
